7c697c944f9bf50901579af31eaaa35f37a57e41
Alstrr <1132246741@pfur.ru>
2024-12-11 00:38:11 +0300

    Create lab_12_42.c

---
+++ lab 12/lab_12_42.c
@@ -0,0 +1,34 @@
+/* Определить структуру с именем Сoat содержащую следующие поля: страна
+производитель; размер шубы; материал, из которого изготовлен; цена. */
+
+#include <stdio.h>
+#include <locale.h>
+
+struct Coat {
+    char country[50];
+    char manufacturer[50];
+    char size[10];
+    char material[50];
+    float price;
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    struct Coat myCoat;
+
+    // Пример заполнения полей структуры
+    snprintf(myCoat.country, sizeof(myCoat.country), "Россия");
+    snprintf(myCoat.manufacturer, sizeof(myCoat.manufacturer), "Производитель A");
+    snprintf(myCoat.size, sizeof(myCoat.size), "L");
+    snprintf(myCoat.material, sizeof(myCoat.material), "Шерсть");
+    myCoat.price = 150000;
+
+    // Вывод информации о шубе
+    printf("Страна производитель: %s\n", myCoat.country);
+    printf("Производитель: %s\n", myCoat.manufacturer);
+    printf("Размер: %s\n", myCoat.size);
+    printf("Материал: %s\n", myCoat.material);
+    printf("Цена: %.2f\n", myCoat.price);
+
+    return 0;
+}
