df873caf214a8a6c8e9a663c155438be7ee805a9
Alstrr <1132246741@pfur.ru>
2024-12-11 00:30:27 +0300

    Create lab_4_15.c

---
+++ lab 4/lab_4_15.c
@@ -0,0 +1,24 @@
+/*Ввести с клавиатуры 10 чисел. Если квадрат числа меньше 100, напечатать число и
+его квадрат. */
+
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int number;
+
+    // Ввод 10 чисел
+    printf("Введите 10 чисел:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("Число %d: ", i + 1);
+        scanf("%d", &number);
+        
+        int square = number * number; 
+        if (square < 100) {
+            printf("Число: %d, Квадрат: %d\n", number, square);
+        }
+    }
+
+    return 0;
+}
