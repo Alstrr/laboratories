2dc25a280beaba69ab6cc6cf1a286e6baf647013
Alstrr <1132246741@pfur.ru>
2024-12-17 23:26:15 +0300

    Update lab_12_42.c

--- lab 12/lab_12_42.c
+++ lab 12/lab_12_42.c
@@ -3,27 +3,27 @@
 
 #include <stdio.h>
 #include <locale.h>
-
 struct Coat {
-    char country[50];
-    char manufacturer[50];
-    char size[10];
-    char material[50];
-    float price;
+    char country[50];      
+    char size[10];         
+    char material[50];     
+    float price;            
 };
 
 int main() {
     setlocale(LC_ALL, "Russian");
     struct Coat myCoat;
-    snprintf(myCoat.country, sizeof(myCoat.country), "Россия");
-    snprintf(myCoat.manufacturer, sizeof(myCoat.manufacturer), "Производитель A");
-    snprintf(myCoat.size, sizeof(myCoat.size), "L");
-    snprintf(myCoat.material, sizeof(myCoat.material), "Шерсть");
-    myCoat.price = 150000;
-    printf("Страна производитель: %s\n", myCoat.country);
-    printf("Производитель: %s\n", myCoat.manufacturer);
-    printf("Размер: %s\n", myCoat.size);
-    printf("Материал: %s\n", myCoat.material);
+    printf("Введите страну производителя: ");
+    fgets(myCoat.country, sizeof(myCoat.country), stdin);
+    printf("Введите размер шубы: ");
+    fgets(myCoat.size, sizeof(myCoat.size), stdin);
+    printf("Введите материал: ");
+    fgets(myCoat.material, sizeof(myCoat.material), stdin);
+    printf("Введите цену: ");
+    scanf_s("%f", &myCoat.price);
+    printf("Страна: %s", myCoat.country);
+    printf("Размер: %s", myCoat.size);
+    printf("Материал: %s", myCoat.material);
     printf("Цена: %.2f\n", myCoat.price);
 
     return 0;
